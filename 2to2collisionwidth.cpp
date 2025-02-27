3f06e950a9c3dd53af3163b9716292bc161b0d8a
João Viana <57032457+vollous@users.noreply.github.com>
2025-02-27 19:13:55 +0000

    At least it compiles!

---
+++ 2to2collisionwidth.cpp
@@ -0,0 +1,391 @@
+#include "2to2collisionwidth.hpp"
+
+using namespace TwoToTwoCollisionWidth;
+
+double Process::integrate_phi(const double &theta) {
+  gsl_integration_workspace *w = gsl_integration_workspace_alloc(1000);
+
+  auto ptr = [=](const double &phi) -> double {
+    return integrand_theta_phi(theta, phi);
+  };
+  gsl_function_pp<decltype(ptr)> Fp(ptr);
+  const gsl_function *F = static_cast<gsl_function *>(&Fp);
+
+  double result, error;
+  gsl_integration_qag(F, 0, M_PI, 1e-7, 1e-7, 1000, GSL_INTEG_GAUSS15, w,
+                      &result, &error);
+  gsl_integration_workspace_free(w);
+  return result;
+}
+
+double Process::Integrate(const double &E1, const double &ET, const double &s,
+                          const std::vector<double> &p1,
+                          const std::vector<double> &p1p2) {
+  E1_ = E1;
+  ET_ = ET;
+  s_ = s;
+  p1_ = p1;
+  p1p2_ = p1p2;
+  p12_ = p1p2_ * p1p2_;
+
+  gsl_integration_workspace *w = gsl_integration_workspace_alloc(1000);
+  gsl_function F;
+
+  // Capture 'this' using another lambda
+  auto lambda = [](double theta, void *params) -> double {
+    return static_cast<Process *>(params)->integrate_phi(theta);
+  };
+
+  F.function = lambda;
+  F.params = this;
+
+  double result, error;
+  gsl_integration_qag(&F, 0, 2 * M_PI, 1e-7, 1e-7, 1000, GSL_INTEG_GAUSS15, w,
+                      &result, &error);
+  gsl_integration_workspace_free(w);
+  return result;
+}
+
+double Process::Energy(const double &m, const std::vector<double> &p) {
+  return sqrt(pow(m, 2) + pow(p[0], 2) + pow(p[1], 2) + pow(p[2], 2));
+}
+
+double Process::Distribution(const double &m, const std::vector<double> &p,
+                             const int &s) {
+  return 1 / (exp(Energy(m, p) / T) + s);
+}
+
+void Process::calculate_r(double &r, double &delta_r, double shift,
+                          double theta, double phi, const double &rcoeff) {
+
+  double sqrtpart = sqrt(
+      pow(rcoeff, 2) * pow(-4 * pow(ET_, 2) + 4 * pow(m3, 2) - 4 * pow(m4, 2) +
+                               4 * p1p2_ * p1p2_ + 8 * pow(ET_, 2) * shift -
+                               8 * p1p2_ * p1p2_ * shift,
+                           2) -
+      4 * (4 * pow(ET_, 2) - 4 * pow(rcoeff, 2)) *
+          (-pow(ET_, 4) + 2 * pow(ET_, 2) * pow(m3, 2) - pow(m3, 4) +
+           2 * pow(ET_, 2) * pow(m4, 2) + 2 * pow(m3, 2) * pow(m4, 2) -
+           pow(m4, 4) + 2 * pow(ET_, 2) * p1p2_ * p1p2_ -
+           2 * pow(m3, 2) * p1p2_ * p1p2_ + 2 * pow(m4, 2) * p1p2_ * p1p2_ -
+           pow(p1p2_ * p1p2_, 2) - 4 * pow(ET_, 2) * p1p2_ * p1p2_ * shift +
+           4 * pow(m3, 2) * p1p2_ * p1p2_ * shift -
+           4 * pow(m4, 2) * p1p2_ * p1p2_ * shift +
+           4 * pow(p1p2_ * p1p2_, 2) * shift +
+           4 * pow(ET_, 2) * p1p2_ * p1p2_ * pow(shift, 2) -
+           4 * pow(p1p2_ * p1p2_, 2) * pow(shift, 2)));
+
+  if (sqrtpart < 0) {
+    std::cout << "Check\t"
+              << (pow(4 * pow(m3, 2) - 4 * pow(m4, 2), 2) * pow(rcoeff, 2) -
+                  4 *
+                      (-4 * pow(ET_, 4) + 8 * pow(ET_, 2) * pow(m3, 2) -
+                       4 * pow(m3, 4) + 8 * pow(ET_, 2) * pow(m4, 2) +
+                       8 * pow(m3, 2) * pow(m4, 2) - 4 * pow(m4, 4) +
+                       4 * pow(ET_, 2) * p12_) *
+                      (16 * pow(ET_, 2) - pow(rcoeff, 2)))
+              << "\n";
+    std::cout << "\nCannot calculate rr\n\n";
+    std::cout << "shift\t" << shift << "\n";
+    std::cout << "p1_\t" << p1_[0] << "\t" << p1_[1] << "\t" << p1_[2] << "\t"
+              << "\n";
+    std::cout << "p2_\t" << (p1p2_ - p1_)[0] << "\t" << (p1p2_ - p1_)[1] << "\t"
+              << (p1p2_ - p1_)[2] << "\t"
+              << "\n";
+    std::cout << "p1 + p2\t" << p1p2_[0] << "\t" << p1p2_[1] << "\t" << p1p2_[2]
+              << "\n";
+    std::cout << "p1 . p2\t" << p12_ << "\n";
+    std::cout << "theta\t" << theta << "\n";
+    std::cout << "phi\t" << phi << "\n";
+    std::cout << "rcoeff\t" << rcoeff << "\n";
+    std::cout << "Spare energy\t" << ET_ - Energy(m3 + m4, p1p2_) << "\n";
+    std::cout << "p12_\t" << p12_ << "\n";
+    std::cout << "ET_\t" << ET_ << "\n";
+    std::cout << "m3\t" << m3 << "\n";
+    std::cout << "m4\t" << m4 << "\n";
+
+    throw;
+  };
+
+  r = (-(rcoeff * (-4 * pow(ET_, 2) + 4 * pow(m3, 2) - 4 * pow(m4, 2) +
+                   4 * p1p2_ * p1p2_ + 8 * pow(ET_, 2) * shift -
+                   8 * p1p2_ * p1p2_ * shift)) +
+       sqrtpart) /
+      (2. * (4 * pow(ET_, 2) - 4 * pow(rcoeff, 2)));
+
+  delta_r =
+      abs(-0.5 * (2 * r + 2 * rcoeff * (-1 + shift)) /
+              sqrt(pow(m3, 2) + pow(r, 2) + 2 * rcoeff * r * (-1 + shift) +
+                   p1p2_ * p1p2_ * pow(-1 + shift, 2)) -
+          (2 * r + 2 * rcoeff * shift) /
+              (2. * sqrt(pow(m4, 2) + pow(r, 2) + 2 * rcoeff * r * shift +
+                         p1p2_ * p1p2_ * pow(shift, 2))));
+}
+
+double Process::integrand_theta_phi(const double &theta, const double &phi) {
+
+  const std::vector<double> p4_centered = {cos(theta) * sin(phi),
+                                           sin(theta) * sin(phi), cos(phi)};
+
+  const double rcoeff = p4_centered * p1p2_;
+  double r, delta_r;
+  const double shift = (pow(ET_, 2) - pow(m3, 2) + pow(m4, 2) - p1p2_ * p1p2_) /
+                       (2 * pow(ET_, 2) - 2 * p1p2_ * p1p2_);
+  calculate_r(r, delta_r, shift, theta, phi, rcoeff);
+  if (r < 0) {
+    std::cout << "Negative r\n";
+    throw;
+  }
+
+  const std::vector<double> p4 = shift * p1p2_ + r * p4_centered;
+  const std::vector<double> p3 = p1p2_ - p4;
+
+  if (abs(ET_ / (Energy(m3, p3) + Energy(m4, p4))) - 1 > 1e-8) {
+    std::cout << "------------------\tEnergy mismatch\t---------------------\n";
+    std::cout << "r\t" << r << "\n";
+    std::cout << "shift\t" << shift << "\n";
+    std::cout << "rcoeff\t" << rcoeff << "\n";
+    std::cout << "p12\t" << p12_ << "\n";
+    std::cout << "delta_r\t" << delta_r << "\n";
+    std::cout << "p1\t" << p1_[0] << "\t" << p1_[1] << "\t" << p1_[2] << "\n";
+    std::cout << "p2\t" << (p1p2_ - p1_)[0] << "\t" << (p1p2_ - p1_)[1] << "\t"
+              << (p1p2_ - p1_)[2] << "\n";
+    std::cout << "p3\t" << p3[0] << "\t" << p3[1] << "\t" << p3[2] << "\n";
+    std::cout << "p4\t" << p4[0] << "\t" << p4[1] << "\t" << p4[2] << "\n";
+
+    std::cout << "\n--------\n";
+
+    std::cout << "E1\t" << E1_ << "\n";
+    std::cout << "E2\t" << ET_ - E1_ << "\n";
+    std::cout << "E3\t" << Energy(m3, p3) << "\n";
+    std::cout << "E4\t" << Energy(m4, p4) << "\n";
+
+    std::cout << "\nE1 + E2\t" << ET_ << "\n";
+    std::cout << "E3 + E4\t" << Energy(m3, p3) + Energy(m4, p4) << "\n";
+
+    std::cout << "--------\n";
+
+    std::cout << "m1\t" << m1 << "\n";
+    std::cout << "m2\t" << m2 << "\n";
+    std::cout << "m3\t" << m3 << "\n";
+    std::cout << "m4\t" << m4 << "\n";
+
+    std::cout << "--------\n";
+
+    std::cout << "theta\t" << theta << "\n";
+    std::cout << "phi\t" << phi << "\n";
+
+    throw;
+  }
+
+  const double t =
+      pow(m1, 2) + pow(m3, 2) - 2 * E1_ * Energy(m3, p3) + p1_ * p3;
+
+  return AmplitudeSquared(s_, t) * Distribution(m3, p3, s3) *
+         Distribution(m4, p4, s4) * r * r * sin(phi) /
+         (delta_r * 4 * Energy(m3, p3) * Energy(m4, p4));
+}
+
+double Process::MonteCarloInt(const std::vector<double> p1,
+                              const std::vector<double> p2) {
+
+  const std::vector<double> p1p2 = p1 + p2;
+  const double p1dotp2 = p1 * p2;
+  const double E1 = Energy(m1, p1);
+  const double E2 = Energy(m2, p2);
+  const double s = m1 * m1 + m2 * m2 + 2 * E1 * E2 - 2 * p1dotp2;
+
+  // Theta function to see if its possible to have a
+  // process with a given p1 and p2
+  if (E1 + E2 - Energy(m3 + m4, p1p2) <= 0)
+    return 0.;
+
+  double integral = Integrate(E1, E1 + E2, s, p1, p1p2);
+
+  // Normalization
+  integral *=
+      Distribution(m1, p1, s1) * Distribution(m2, p2, s2) / (4 * E1 * E2);
+
+  return integral;
+};
+
+Process::Process(const double &T_in, const int &s1_in, const int &s2_in,
+                 const int &s3_in, const int &s4_in, const double &m1_in,
+                 const double &m2_in, const double &m3_in, const double &m4_in)
+    : s1(s1_in), s2(s2_in), s3(s3_in), s4(s4_in), T(T_in), m1(m1_in), m2(m2_in),
+      m3(m3_in), m4(m4_in) {};
+
+struct tLgTOtRH : Process {
+  using Process::Process; // Import constructor
+
+  double AmplitudeSquared(const double &s, const double &t) override {
+    const double e = 0.31;
+    const double gs = 1.;
+    const double sw = 0.472;
+    const double mw = 80;
+
+    return (2 * pow(e, 2) * pow(gs, 2) * pow(m1, 2) *
+            (-(pow(m1, 6) *
+               (pow(m2, 4) + pow(m2, 2) * (-3 * pow(m3, 2) +
+                                           17 * (2 * pow(m1, 2) + pow(m2, 2) +
+                                                 pow(m3, 2) - s - t)))) +
+             pow(m1, 4) *
+                 (pow(m2, 4) *
+                      (2 * pow(m3, 2) + s -
+                       4 * (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s - t) +
+                       4 * t) +
+                  pow(m2, 2) *
+
+                      (-3 * pow(m3, 4) +
+                       pow(m3, 2) * (-9 * pow(m3, 2) +
+                                     4 * (2 * pow(m1, 2) + pow(m2, 2) +
+                                          pow(m3, 2) - s - t) -
+                                     6 * t) +
+                       pow(m3, 2) * (10 * (2 * pow(m1, 2) + pow(m2, 2) +
+                                           pow(m3, 2) - s - t) +
+                                     t) +
+                       (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s - t) *
+                           (2 * (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s -
+                                 t) +
+                            31 * t))) +
+             t * (-(pow(m2, 4) * s * (2 * pow(m3, 2) - 3 * t)) -
+                  pow(m2, 2) *
+                      (pow(m3, 6) + pow(m3, 4) * t -
+                       pow(m3, 2) *
+                           (2 * t *
+                                (3 * (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) -
+                                      s - t) +
+                                 t) +
+                            pow(m3, 2) * (-2 * pow(m1, 2) - pow(m2, 2) -
+                                          pow(m3, 2) + s + 2 * t)) +
+                       (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s) *
+                           (pow(m3, 2) * (-2 * pow(m1, 2) - pow(m2, 2) -
+                                          pow(m3, 2) + s + 2 * t) +
+                            (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s - t) *
+                                (-2 * pow(m1, 2) - pow(m2, 2) - pow(m3, 2) + s +
+                                 4 * t)))) +
+             pow(m1, 2) *
+                 (pow(m2, 4) *
+                      (4 * s *
+                           (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s - t) -
+                       4 * s * t - 3 * pow(t, 2) + pow(m3, 2) * (-3 * s + t) +
+                       pow(m3, 2) * (s + t)) +
+                  pow(m2, 2) *
+                      (5 * pow(m3, 6) -
+                       4 * pow(m3, 4) *
+                           (2 * (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s -
+                                 t) +
+                            3 * t) +
+                       pow(m3, 2) * (-pow(2 * pow(m1, 2) + pow(m2, 2) +
+                                              pow(m3, 2) - s - t,
+                                          2) -
+                                     10 *
+                                         (2 * pow(m1, 2) + pow(m2, 2) +
+                                          pow(m3, 2) - s - t) *
+                                         t +
+                                     pow(t, 2)) -
+                       (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s - t) *
+                           (pow(2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s -
+                                    t,
+                                2) +
+                            16 *
+                                (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s -
+                                 t) *
+                                t +
+                            11 * pow(t, 2)) +
+                       pow(m3, 2) *
+                           (pow(m3, 2) * (2 * pow(m1, 2) + pow(m2, 2) +
+                                          pow(m3, 2) - s + 7 * t) +
+                            2 * (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) - s - t) *
+                                (2 * (2 * pow(m1, 2) + pow(m2, 2) + pow(m3, 2) -
+                                      s - t) +
+                                 11 * t)))))) /
+           (pow(m2, 2) * pow(mw, 2) * pow(-pow(m1, 2) + s, 2) * pow(sw, 2) *
+            pow(-pow(m1, 2) + t, 2));
+  }
+};
+
+static int Integrand(const int *ndim, const cubareal xx[], const int *ncomp,
+                     cubareal ff[], void *userdata) {
+  auto proc = static_cast<tLgTOtRH *>(userdata);
+
+  if (xx[0] == 1 or xx[3] == 1) {
+    // TODO might be removable
+    ff[0] = 0.;
+    return 0;
+  }
+
+  const double scalling = 100.; // Put the hypercube on the region of interest.
+
+  // Spherical coordinates p1
+  const double r1 = scalling * xx[0] / (1 - xx[0]);
+  const double theta1 = 2 * M_PI * xx[1];
+  const double phi1 = M_PI * xx[2];
+  const std::vector<double> p1 =
+      r1 * (std::vector<double>){cos(theta1) * sin(phi1),
+                                 sin(theta1) * sin(phi1), cos(phi1)};
+
+  // Spherical coordinates p2
+  const double r2 = scalling * xx[3] / (1 - xx[3]);
+  const double theta2 = 2 * M_PI * xx[4];
+  const double phi2 = M_PI * xx[5];
+  const std::vector<double> p2 =
+      r2 * (std::vector<double>){cos(theta2) * sin(phi2),
+                                 sin(theta2) * sin(phi2), cos(phi2)};
+  // Return result
+  // std::cout << "p1 >\t" << p1[0] << "\t" << p1[1] << "\t" << p1[2] << "\t";
+  // std::cout << "p2 >\t" << p2[0] << "\t" << p2[1] << "\t" << p2[2] << "\t";
+
+  ff[0] = proc->MonteCarloInt(p1, p2) * _4_M_4 * (pow(r1, 2) + 1) *
+          (pow(r2, 2) + 1) / (scalling * scalling);
+
+  // std::cout << "r >\t" << ff[0] << "\n";
+  return 0;
+};
+
+int main() {
+  double T = 100.;
+
+  int s1 = 1;
+  int s2 = -1;
+  int s3 = 1;
+  int s4 = -1;
+
+  double m1 = 170;
+  double m2 = 10;
+  double m3 = 170;
+  double m4 = 100;
+
+  tLgTOtRH proc(T, s1, s2, s3, s4, m1, m2, m3, m4);
+
+  const int NDIM = 3;
+  const int NCOMP = 1;
+  const int NVEC = 20;
+  const double EPSREL = 1e-6;
+  const double EPSABS = 1e-12;
+  const int VERBOSE = 3;
+  const int LAST = 0;
+  const int SEED = 0;
+  const int MINEVAL = 0;
+  const int MAXEVAL = 100000;
+  const int NSTART = 10000;
+  const int NINCREASE = 1000;
+  const int NBATCH = 1000;
+  const int GRIDNO = 0;
+  const char *STATEFILE = NULL;
+  const int *SPIN = NULL;
+
+  int comp, nregions, neval, fail;
+  cubareal integral[NCOMP], error[NCOMP], prob[NCOMP];
+
+  Vegas(NDIM, NCOMP, Integrand, &proc, NVEC, EPSREL, EPSABS, VERBOSE, SEED,
+        MINEVAL, MAXEVAL, NSTART, NINCREASE, NBATCH, GRIDNO, STATEFILE, &SPIN,
+        &neval, &fail, integral, error, prob);
+
+  std::cout << "Integral\t" << integral[0] << "\n";
+  std::cout << "Error \t" << error[0] << "\n";
+  std::cout << "Relative error \t" << error[0] / integral[0] << "\n";
+  std::cout << "prob\t" << prob[0] << "\n";
+
+  return 0;
+}
---
+++ 2to2collisionwidth.hpp
@@ -0,0 +1,218 @@
+#pragma once
+
+#ifndef TWOTOTWOCOLLISIONWIDTH_HPP
+#define TWOTOTWOCOLLISIONWIDTH_HPP
+
+#include <assert.h>
+#include <cmath>
+#include <cuba.h>
+#include <functional>
+#include <gsl/gsl_integration.h>
+#include <iostream>
+#include <numeric>
+#include <string>
+
+#define _4_M_4 4 * M_PI *M_PI *M_PI *M_PI /* 4 pi^4 */
+
+namespace TwoToTwoCollisionWidth {
+
+/**
+ * @brief vector addition
+ */
+template <typename T>
+std::vector<T> operator+(const std::vector<T> &a, const std::vector<T> &b) {
+  assert(a.size() == b.size());
+  std::vector<T> result;
+  result.reserve(a.size());
+
+  std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(result),
+                 std::plus<T>());
+  return result;
+}
+
+/**
+ * @brief vector subtraction
+ */
+template <typename T>
+std::vector<T> operator-(const std::vector<T> &a, const std::vector<T> &b) {
+  assert(a.size() == b.size());
+
+  std::vector<T> result;
+  result.reserve(a.size());
+
+  std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(result),
+                 std::minus<T>());
+  return result;
+}
+
+/**
+ * @brief multiplication of vector with scalar
+ */
+template <typename T, typename T2>
+std::vector<T> operator*(const T2 &a, const std::vector<T> &b) {
+  std::vector<T> result;
+  result.reserve(b.size());
+
+  std::transform(b.begin(), b.end(), std::back_inserter(result),
+                 [&a](T i) { return a * i; });
+  return result;
+}
+
+/**
+ * @brief division of vector by scalar
+ */
+template <typename T, typename T2>
+std::vector<T> operator/(const std::vector<T> &a, const T2 &b) {
+  std::vector<T> result;
+  result.reserve(a.size());
+
+  std::transform(a.begin(), a.end(), std::back_inserter(result),
+                 [&b](T i) { return i / b; });
+  return result;
+}
+
+/**
+ * @brief dot product of two vectors
+ */
+template <typename T>
+T operator*(const std::vector<T> &a, const std::vector<T> &b) {
+  assert(a.size() == b.size());
+
+  std::vector<T> result;
+  result.reserve(a.size());
+
+  std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(result),
+                 [](T i, T j) { return (i * j); });
+
+  T result1 = std::accumulate(result.begin(), result.end(), 0.0);
+
+  return result1;
+}
+
+template <typename F> class gsl_function_pp : public gsl_function {
+public:
+  gsl_function_pp(const F &func) : _func(func) {
+    function = &gsl_function_pp::invoke;
+    params = this;
+  }
+
+private:
+  const F &_func;
+  static double invoke(double x, void *params) {
+    return static_cast<gsl_function_pp *>(params)->_func(x);
+  }
+};
+
+struct Process {
+
+  std::vector<double> p1p2_;
+  std::vector<double> p1_;
+  double E1_, ET_, s_, p12_;
+  const int s1, s2, s3, s4;
+  const double m1, m2, m3, m4, T;
+
+  /**
+   * @brief Calculate energy
+   *
+   * @param m mass
+   * @param p momentum
+   * @return double
+   */
+  static double Energy(const double &m, const std::vector<double> &p);
+
+  /**
+   * @brief Calculate the probability density
+   *
+   * @param m mass
+   * @param p momentum
+   * @param s -1 for boson, 1 for fermion
+   * @return double
+   */
+  double Distribution(const double &m, const std::vector<double> &p,
+                      const int &s);
+
+  /**
+   * @brief Calculate r for a given rcoeff and shift. theta and phi are not used
+   * in this routine
+   *
+   * @param r radius
+   * @param delta_r derivative of inside delta function
+   * @param shift shift to center the potato
+   * @param theta angle with the x axis
+   * @param phi angle with the z acis
+   * @param rcoeff (p1 + p2) . director( p4 )
+   */
+  void calculate_r(double &r, double &delta_r, double shift, double theta,
+                   double phi, const double &rcoeff);
+
+  /**
+   * @brief Integrates over the outgoing momentum for a given p1 and p2.
+   * Integrates over the out going angles
+   *
+   * @param p1 momentum of particle 1
+   * @param p2 momentum of particle 2
+   * @return double
+   */
+  double MonteCarloInt(const std::vector<double> p1,
+                       const std::vector<double> p2);
+
+  /**
+   * @brief Integrand over theta and phi
+   *
+   * @param theta x axis angle
+   * @param phi z axis angle
+   * @return double
+   */
+  double integrand_theta_phi(const double &theta, const double &phi);
+
+  /**
+   * @brief Integrates over phi for a given theta
+   *
+   * @param theta
+   * @return double
+   */
+  double integrate_phi(const double &theta);
+
+  /**
+   * @brief
+   *
+   * @param E1 Energy of particle 1
+   * @param ET Energy of particle 1 + Energy of particle 2
+   * @param s s = (p1 + p2)^2 = m1^2 + m2^2 + 2 * E1 * E2 - 2 p1.p2
+   * @param p1 3-momentum of p1
+   * @param p1p2 p1p2 = p1 + p2
+   * @return double
+   */
+  double Integrate(const double &E1, const double &ET, const double &s,
+                   const std::vector<double> &p1,
+                   const std::vector<double> &p1p2);
+
+  /**
+   * @brief Constructor
+   *
+   * @param T_in Temperature
+   * @param s1_in = -1 boson, 1 for fermion. particle 1
+   * @param s2_in = -1 boson, 1 for fermion. particle 2
+   * @param s3_in = -1 boson, 1 for fermion. particle 3
+   * @param s4_in = -1 boson, 1 for fermion. particle 4
+   * @param m1_in mass of particle 1
+   * @param m2_in mass of particle 2
+   * @param m3_in mass of particle 3
+   * @param m4_in mass of particle 4
+   */
+  explicit Process(const double &T_in, const int &s1_in, const int &s2_in,
+                   const int &s3_in, const int &s4_in, const double &m1_in,
+                   const double &m2_in, const double &m3_in,
+                   const double &m4_in);
+
+  /**
+   * @brief Amplitude that depends on s and t only!
+   *
+   * @param s s = (p1 + p2)^2
+   * @param t t = (p1 - p3)^2
+   * @return double
+   */
+  virtual double AmplitudeSquared(const double &s, const double &t) = 0;
+};
+} // namespace TwoToTwoCollisionWidth
+#endif
