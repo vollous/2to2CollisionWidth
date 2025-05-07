146546b18cd422eb2e53b9a5fad5d5e2f0cb3d6f
Vollous <jfvvchico@hotmail.com>
2025-05-07 11:20:37 +0100

    Tidy up the output

--- 2to2.cpp
+++ 2to2.cpp
@@ -401,8 +401,9 @@ int main()
   const double vw    = 0.95;
   const double gamma = 1; /*/ sqrt(1 - pow(vw, 2));*/
   const double N1    = gamma * 2 * pow(M_PI, 3) * pow(T, 2) / 3;
-  std::cout << "\n\nGamma_y = \t" << integral[0] / (N1 * proc.prefactor)
-            << " +- " << error[0] / (N1 * proc.prefactor) << "\n\n";
+  std::cout << "\nT = " << T
+            << " | Gamma_y = " << integral[0] / (N1 * proc.prefactor) << " +- "
+            << error[0] / (N1 * proc.prefactor) << "\n\n";
 
   // Your Code to Execute //
   auto end = std::chrono::system_clock::now();
